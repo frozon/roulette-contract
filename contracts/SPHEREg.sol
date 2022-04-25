69fb5047683b1d9c730c97d17e5efeeb7d76aa65
Thomas <thomas@lauro.fr>
2022-04-25 09:11:16 +0200

    24h locked in & max cash in per 24h

--- contracts/SPHEREg.sol
+++ contracts/SPHEREg.sol
@@ -5,15 +5,26 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract cSphere is ERC20, AccessControl {
+contract SPHEREg is ERC20, AccessControl {
   bytes32 public constant GAME_ROLE = keccak256("GAME");
+
+  address public sphereToken;
 
   address constant DEAD = 0x000000000000000000000000000000000000dEaD;
   mapping(address => bool) private games;
   address[] private gamesAddress;
 
-  constructor() ERC20("Casino Sphere", "cSphere") {
+  struct PlayerInfo {
+    uint256 cashedInAt;
+    uint256 cashedInAmount;
+  }
+
+  mapping(address => PlayerInfo) public playerBank;
+
+  constructor(address _sphereToken) ERC20("Sphere Game", "SPHEREg") {
     _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+
+    sphereToken = _sphereToken;
   }
 
   modifier onlyAdmin() {
@@ -55,17 +66,43 @@ contract cSphere is ERC20, AccessControl {
 
   function mint(address _account, uint256 _amount) public onlyGame {
     _mint(_account, _amount);
-
-    // Auto approve???
-    // for(uint256 i = 0; i < gamesAddress.length; i++) {
-    //   _approve(_account, gamesAddress[i], type(uint256).max);
-    // }
   }
 
   function _transfer(address sender, address recipient, uint256 amount) internal override {
     require(hasRole(GAME_ROLE, sender) || hasRole(GAME_ROLE, recipient) || recipient == address(DEAD), "non transferable token between users");
+
+    // Cash out: lock in period, 24h
+    if(hasRole(GAME_ROLE, sender) && recipient != address(DEAD)) {
+      PlayerInfo memory pInfo = playerBank[recipient];
+      require(pInfo.cashedInAt <= block.timestamp + 1 days, "24h locked in period not ended");
+    }
+
+    // Cash in: limit amount to 1 rebase per 24h
+    if(!hasRole(GAME_ROLE, sender)) {
+      PlayerInfo memory pInfo = playerBank[sender];
+      pInfo.cashedInAt = block.timestamp;
+
+      uint256 senderBalance = IERC20(sphereToken).balanceOf(sender);
+
+      // NOTE: super ugly should fetch reward yield from sphere contract
+      uint256 rewardYield = 3943560072416;
+      uint256 rewardYieldDenominator = 10000000000000000;
+      uint256 reward = rewardYield / rewardYieldDenominator;
+
+      uint256 maxCashInAmount = senderBalance * (1.0 + reward) ** 48.0 - senderBalance;
+
+      uint256 cashedInAmount = pInfo.cashedInAt <= block.timestamp + 1 days ? 0 : pInfo.cashedInAmount;
+      require(cashedInAmount + amount <= maxCashInAmount, "can not cash in more than 1 rebase per 24hours");
+
+      pInfo.cashedInAt = block.timestamp;
+      pInfo.cashedInAmount = cashedInAmount;
+    }
+
     return super._transfer(sender, recipient, amount);
   }
 
-  event IAmHere(address sender);
+  function setSphereToken(address _sphereToken) public onlyAdmin {
+    require(_sphereToken != address(0x0), "Sphere Token can not be 0x0");
+    sphereToken = _sphereToken;
+  }
 }
